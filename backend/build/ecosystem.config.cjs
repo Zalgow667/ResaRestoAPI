91cafa4cd4ff784bfd76c3efbe07f06407b71c60
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 01:22:51 +0200

    edit

--- backend/build/ecosystem.config.cjs
+++ backend/build/ecosystem.config.cjs
@@ -2,17 +2,14 @@ module.exports = {
     apps : [{
       script: 'index.js',
       watch: '.'
-    }, {
-      script: './service-worker/',
-      watch: ['./service-worker']
-    }],
+    }, ],
   
     deploy : {
       production : {
         user : 'debian',
         host : '151.80.60.101',
         ref  : 'origin/master',
-        repo : 'https://github.com/Zalgow667/ResaRestoAPI.git',
+        repo : 'git@github.com:Zalgow667/ResaRestoAPI.git',
         path : '/home/debian/server',
         'pre-deploy-local': '',
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.cjs --env production',
