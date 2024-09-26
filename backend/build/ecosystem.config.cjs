65bb8e471f6c779848d3074571974aa846ce07e6
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 01:45:21 +0200

    ntm

--- backend/build/ecosystem.config.cjs
+++ backend/build/ecosystem.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
         host : '151.80.60.101',
         ref  : 'main',
         repo : 'git@github.com:Zalgow667/ResaRestoAPI.git',
-        path : '/home/debian/server/ResaRestoAPI',
+        path : '/home/debian/server/ResaRestoAPI/backend',
         'pre-deploy-local': '',
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.cjs --env production',
         'pre-setup': ''
