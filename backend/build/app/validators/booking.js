ff506a7ecbc4dad190ba14582da86ee6680fd916
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 16:36:11 +0200

    edit config file

--- backend/build/app/validators/booking.js
+++
@@ -1,8 +0,0 @@
-import vine from '@vinejs/vine';
-export const bookingValidator = vine.compile(vine.object({
-    booking_date: vine.date(),
-    start: vine.date(),
-    id_user: vine.number(),
-    id_table: vine.number()
-}));
-//# sourceMappingURL=booking.js.map
--- backend/build/app/validators/table.js
+++
@@ -1,7 +0,0 @@
-import vine from '@vinejs/vine';
-export const newTableValidator = vine.compile(vine.object({
-    numero: vine.number(),
-    capacity: vine.number(),
-    id_plan: vine.number()
-}));
-//# sourceMappingURL=table.js.map
