6ef531ba3bfbd94f825329825a5b705b8e0b11a7
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 14:10:39 +0200

    fix package

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -5,7 +5,6 @@ module.exports = {
       script: './source/backend/build/bin/server.js',
       instances: 'max',
       exec_mode: 'cluster',
-      version: '1.0.0',
       autorestart: true,
       env: {
         NODE_ENV: 'production', 
