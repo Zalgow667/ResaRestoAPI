225302202ad7546fc8a4501d4b57bf35f3954cf5
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 14:01:58 +0200

    fix config

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,26 +1,28 @@
 module.exports = {
-  apps : [
-      {
-        name: 'API',
-        script: './source/backend/build/bin/server.js',
-        instances: 'max',
-        exec_mode: 'cluster',
-        autorestart: true,
-      }, 
-    ],
-  
-    deploy : {
-      production : {
-        user : 'debian',
-        host : '151.80.60.101',
-        NODE_ENV: "production",
-        PORT: 3333,
-        ref  : 'main',
-        repo : 'git@github.com:Zalgow667/ResaRestoAPI.git',
-        path : '/home/debian/server',
-        'pre-deploy-local': '',
-        'post-deploy': 'npm ci && pm2 reload /home/debian/server/ResaRestoAPI/backend/ecosystem.config.cjs --env production',
-        'pre-setup': '',
-      }
+  apps: [
+    {
+      name: 'API',
+      script: './source/backend/build/bin/server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      env: {
+        NODE_ENV: 'production', 
+        PORT: 3333,  
+      },
+    }, 
+  ],
+
+  deploy: {
+    production: {
+      user: 'debian',
+      host: '151.80.60.101',
+      ref: 'main',
+      repo: 'git@github.com:Zalgow667/ResaRestoAPI.git',
+      path: '/home/debian/server',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm ci && pm2 reload /home/debian/server/ResaRestoAPI/backend/ecosystem.config.cjs --env production',
+      'pre-setup': '',
     }
-  };
+  }
+};
