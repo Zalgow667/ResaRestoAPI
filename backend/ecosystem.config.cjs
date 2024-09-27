1df5e847e706b44093332e279732a8108de74669
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 03:28:49 +0200

    add dump

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,8 +1,13 @@
 module.exports = {
-    apps : [{
-      script: 'index.js',
-      watch: '.'
-    }, ],
+  apps : [
+      {
+        name: 'web-app',
+        script: './build/server.js',
+        instances: 'max',
+        exec_mode: 'cluster',
+        autorestart: true,
+      }, 
+    ],
   
     deploy : {
       production : {
@@ -10,7 +15,7 @@ module.exports = {
         host : '151.80.60.101',
         ref  : 'main',
         repo : 'git@github.com:Zalgow667/ResaRestoAPI.git',
-        path : '/home/debian/pm2',
+        path : '/home/debian/server',
         'pre-deploy-local': '',
         'post-deploy': 'npm install && pm2 reload /home/debian/server/ResaRestoAPI/backend/ecosystem.config.cjs --env production',
         'pre-setup': ''
