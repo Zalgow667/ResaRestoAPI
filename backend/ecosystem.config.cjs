2f29a90aabde64ba540747f085d77ac2f763701f
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 15:56:07 +0200

    fix files

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,16 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: 'backend',
-      script: './source/backend/build/bin/server.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      watch: true,
-      autorestart: true,
-      env: {
-        NODE_ENV: 'production', 
-        PORT: 80,  
-      },
-    }, 
-  ],
+  apps : [{
+    script: './source/backend/build/bin/server.js',
+  }],
+
+  deploy : {
+    production : {
+      key : 'key.pem',
+      user : 'debian',
+      host : '151.80.60.101',
+      ref : 'main',
+      repo : 'git@github.com:Zalgow667/ResaRestoAPI.git',
+      path : '/home/debian',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm ci && npm run build && pm2 reload /home/debian/server/ResaRestoAPI/backend/ecosystem.config.cjs --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
 };
