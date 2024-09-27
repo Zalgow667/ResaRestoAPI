39106c29c209835aa44b6b9cb12f69db5be2eca1
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 14:35:59 +0200

    russian tuto

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -5,24 +5,12 @@ module.exports = {
       script: './source/backend/build/bin/server.js',
       instances: 'max',
       exec_mode: 'cluster',
+      watch: true,
       autorestart: true,
       env: {
         NODE_ENV: 'production', 
-        PORT: 3333,  
+        PORT: 80,  
       },
     }, 
   ],
-
-  deploy: {
-    production: {
-      user: 'debian',
-      host: '151.80.60.101',
-      ref: 'main',
-      repo: 'git@github.com:Zalgow667/ResaRestoAPI.git',
-      path: '/home/debian/server',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm ci && pm2 reload /home/debian/server/ResaRestoAPI/backend/ecosystem.config.cjs --env production',
-      'pre-setup': '',
-    }
-  }
 };
