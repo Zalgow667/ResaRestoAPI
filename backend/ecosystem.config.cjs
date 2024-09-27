79bb51fabb31169b4f377e0d168b5a7b40312d7d
Zalgow667 <118558870+Zalgow667@users.noreply.github.com>
2024-09-27 13:56:08 +0200

    fix config

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -13,16 +13,14 @@ module.exports = {
       production : {
         user : 'debian',
         host : '151.80.60.101',
+        NODE_ENV: "production",
+        PORT: 3333,
         ref  : 'main',
         repo : 'git@github.com:Zalgow667/ResaRestoAPI.git',
         path : '/home/debian/server',
         'pre-deploy-local': '',
         'post-deploy': 'npm install && pm2 reload /home/debian/server/ResaRestoAPI/backend/ecosystem.config.cjs --env production',
         'pre-setup': '',
-        env: {
-          NODE_ENV: "production",
-          PORT: 3333   
-        }
       }
     }
   };
